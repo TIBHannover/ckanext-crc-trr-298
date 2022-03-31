a9ecca1b60b75e9fa6e638941009077c03d53669
pooya.oa <pooya.oladazimi@tib.eu>
2022-03-31 10:13:27 +0200

    added check plugin enabled function

--- ckanext/sfb_layout/lib.py
+++ ckanext/sfb_layout/lib.py
@@ -24,4 +24,12 @@ class Helper():
             # localhost
             return '1368'
     
+
+    @staticmethod
+    def check_plugin_enabled(plugin_name):
+        plugins = toolkit.config.get("ckan.plugins")
+        if plugin_name in plugins:
+            return True
+        return False
+    
     
