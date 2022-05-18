1b70b603e4b07b3195adbe50230ab377556d3892
pooya.oa <pooya.oladazimi@tib.eu>
2022-05-18 12:46:54 +0200

    do not activate the sfb search on org/group searc

--- ckanext/sfb_layout/public/sfb_layout/statics/search.js
+++ ckanext/sfb_layout/public/sfb_layout/statics/search.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
     $('#search-type-dropdown').select2({ width: '100%' });
 
-    $('.search-form').submit(function(e){
-        e.preventDefault();
-        let searchType = $('#search-type-dropdown').val();
-        let searchPhrase = $('#field-giant-search-mimic').val();
-        if (searchType !== '0'){
-            $('#field-giant-search').val('');
-            $('#field-giant-search').val(searchType + ":" + searchPhrase);
+    $('.search-form').submit(function(e){        
+        if($('#search-type-dropdown').length !== 0){
+            e.preventDefault();
+            let searchType = $('#search-type-dropdown').val();
+            let searchPhrase = $('#field-giant-search-mimic').val();
+            if (searchType !== '0'){
+                $('#field-giant-search').val('');
+                $('#field-giant-search').val(searchType + ":" + searchPhrase);
+            }
+            else{
+                $('#field-giant-search').val('');
+                $('#field-giant-search').val(searchPhrase);
+            }
+            $('.search-form')[0].submit();
         }
-        else{
-            $('#field-giant-search').val('');
-            $('#field-giant-search').val(searchPhrase);
-        }
-        $('.search-form')[0].submit();
+        $('.search-form')[0].submit();        
     });
 });
